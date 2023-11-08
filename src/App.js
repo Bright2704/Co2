231e74961f4d6ec8cb6f9656b051c49ca1ace50c
Bright2704 <thanonchaikantha27@gmail.com>
2023-11-08 09:43:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import React from 'react';
 import './background1.css'
 import './background2.css'
 import './background3.css'
+import './logo1.css'
+import logoImage from './logo1.jpg';
+
 
 const Section = ({ className, title, children }) => (
   <section className={className}>
@@ -17,17 +20,26 @@ const Section = ({ className, title, children }) => (
 
 
 function App() {
+  
   return (
     <div > 
       <div className="background1"> 
-      <Navbar/>
+      <div>
+      
+      <Navbar />
+      
+     
+      
+    </div>
       <Section>
       
        <div style={{
+        marginTop: "300px",
         marginLeft: "50px",
         marginRight: "100px",}}>
-          <h1>Welcome to Our Website</h1>
+          
           <p>This is some content inside the 'Welcome' section.</p>
+          <h1>Welcome to Our Website</h1>
           <a className="btn animate" href="/launches/mission/?missionId=sl-6-26" aria-label="REWATCH Starlink mission" role="button" style={{
     opacity: 1,
     visibility: 'inherit',
@@ -39,17 +51,24 @@ function App() {
         </div>
       </Section>
       </div>
-
-
-      <Section className="background2" title="Welcome to Our Website">
-
+      
 <div style={{
-  marginTop: "300px",
-  marginLeft: "50px",
+  marginTop: "0px",
+}}></div>
+     <Section className="background2" >
+     <h2>
+    <section id="about" className="section">
+      <img src={require('./logo1.jpg')} alt="About" className="logo1" />
+    </section>
+  </h2>
+      <div style={{
+  marginTop: "400px",
+  marginLeft: "20pa",
   marginRight: "100px",
 }}>
-  <h1>Welcome to Our Website</h1>
+  
   <p>This is some content inside the 'Welcome' section.</p>
+  <h1>Welcome to Our Website</h1>
   <a className="btn animate" href="/launches/mission/?missionId=sl-6-26" aria-label="REWATCH Starlink mission" role="button" style={{
     opacity: 1,
     visibility: 'inherit',
@@ -62,18 +81,27 @@ function App() {
 
 </Section>
 
-      <Section className="background3" title="Welcome to Our Website">
+      <Section className="background3"  > 
+
+      <section id="pricing" className="section white-text">
+  <img src={logoImage} alt="Pricing" className="logo1" />
+  {/* ... rest of your pricing section content ... */}
+</section>
+ 
       <div style={{
-  marginTop: "300px",
+  marginTop: "400px",
   marginLeft: "50px",
   marginRight: "100px",
 }}>
-  <h1>Welcome to Our Website</h1>
-  <p>This is some content inside the 'Welcome' section.</p>
+  
+<p style={{ color: 'white' }}>This is some content inside the 'Welcome' section.</p>
+<h1 style={{ color: 'white' }}>Welcome to Our Website</h1>
   <a className="btn animate" href="/launches/mission/?missionId=sl-6-26" aria-label="REWATCH Starlink mission" role="button" style={{
-    opacity: 1,
-    visibility: 'inherit',
-    transform: 'translate3d(0px, 0px, 0px)'
+       color: 'white',
+       opacity: 1,
+       visibility: 'inherit',
+       transform: 'translate3d(0px, 0px, 0px)'
+   
   }}>
     <div className="hover"></div>
     <span className="text">REWATCH</span>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,71 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Assuming your styles are defined here
+import React from 'react';
+import './logo1.css'
+import logoImage from './logo1.jpg'; //
 
 
 function Navbar() {
   return (
-    <div className="App">
-<nav className="navbar navbar-expand-lg transparent-navbar">
-      <div class="container-fluid">
-    <a class="navbar-brand" href="#">LOGO</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+
+    <nav className="navbar navbar-expand-lg transparent-navbar">
+    <div className="container-fluid">
+    <a className="navbar-brand" href="#home">
+    <img src={logoImage} alt="LOGO" className="logo1" />
+        </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#home">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#pricing">Pricing</a>
+          </li>
+          {/* You can add a disabled link or more functional links as needed */}
+        </ul>
+        
+        
+      </div>
     </div>
-  </div>
-</nav>
-    </div>
-  );
+  </nav>
+
+ 
+
+   );
 }
 
 export default Navbar;
+
+
+//     <div className="App">
+// <nav className="navbar navbar-expand-lg transparent-navbar">
+//       <div class="container-fluid">
+//     <a class="navbar-brand" href="#">LOGO</a>
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//     <div class="collapse navbar-collapse" id="navbarNav">
+//       <ul class="navbar-nav">
+//         <li class="nav-item">
+//           <a class="nav-link active" aria-current="page" href="#">Home</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">About</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Pricing</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+//     </div>
